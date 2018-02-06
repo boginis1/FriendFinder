116dd8027e7402b632968c983a72657fc9b5420e
boginis1 <hollyzoba@gmail.com>
2018-02-05 20:12:40 -0700

    structure built

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+// {
+//   "name":"Ahmed",
+//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//   "scores":[
+//       5,
+//       1,
+//       4,
+//       4,
+//       5,
+//       1,
+//       2,
+//       5,
+//       4,
+//       1
+//     ]
+// }
+
+var friendsArray = [
+  {
+    FriendID: "1",
+    name: "Friend Name",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 5]
+   },
+ ];
---
+++ server.js
@@ -0,0 +1,78 @@
+// Your survey should have 10 questions of your choosing. Each answer should be on a scale of 1 to 5 based on how much the user agrees or disagrees with a question.
+
+
+
+
+
+// Determine the user's most compatible friend using the following as a guide:
+
+// Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
+// With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
+
+
+// Example: 
+
+
+// User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+
+// User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+
+// Total Difference: 2 + 1 + 2 = 5
+
+
+// Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on. 
+// The closest match will be the user with the least amount of difference.
+
+
+// Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+
+// The modal should display both the name and picture of the closest match. 
+
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+
+   
+   
+   
+var app = express();
+var port = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require('./apiRoutes.js')(app);
+require('./htmlRoutes.js')(app);
+// Basic route that sends the user first to the AJAX Page
+
+// Search for Specific Character (or all characters) - provides JSON
+// app.get("/api/:tables?", function(req, res) {
+//   var chosen = req.params.tables;
+
+//   if (chosen) {
+//     console.log(chosen);
+
+//     for (var i = 0; i < tables.length; i++) {
+//       if (chosen === tables[i].routeName) {
+//         return res.json(tables[i]);
+//       }
+//     }
+//     return res.json(false);
+//   }
+//   return res.json(tables);
+// });
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
